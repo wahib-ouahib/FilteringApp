52eb2b2a5c261dbf76d35cbc6bcb040897706d32
wahib <wahib.med.ouahib@gmail.com>
2018-10-30 22:28:44 +0000

    :+1: adding event listener to the serch tab

---
+++ filterablelist/app.js
@@ -0,0 +1,9 @@
+
+//Get Input Element
+let filterInput = document.getElementById("filterInput");
+//Add event listener
+filterInput.addEventListener('keyup', filterNames);
+
+function filterNames(){
+    console.log(1);
+}
