3882d348dc40f8054e7299b3e021ff9c9d9e4a66
wahib <wahib.med.ouahib@gmail.com>
2018-10-31 17:03:45 +0000

    :hammer: Dynamize the web app with JavaScript

--- filterablelist/app.js
+++ filterablelist/app.js
@@ -7,7 +7,14 @@ filterInput.addEventListener('keyup', filterNames);
 function filterNames(){
     // Get value of input
 
-    let filterValue = document.getElementById("filterInput").innerHTML;
+    let filterValue = document.getElementById("filterInput").value.toUpperCase();
+
+    // Get names from ul
+    let ul = document.getElementById('names');
+    // Get lists from ul
+    let li = ul.querySelectorAll('li.collection-item');
+    console.log(li);
+    //loop through collection-item lists
+    
    
 }
-console.log(filterNames());
