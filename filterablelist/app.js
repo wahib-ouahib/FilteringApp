7004c5bc21f3b1264a2c5620e74f79f3acf95fe1
wahib <wahib.med.ouahib@gmail.com>
2018-10-31 22:01:42 +0000

    :hammer: Optimizing Js

--- filterablelist/app.js
+++ filterablelist/app.js
@@ -1,29 +1,26 @@
-
-//Get Input Element
-let filterInput = document.getElementById("filterInput");
-//Add event listener
+// Get input element
+let filterInput = document.getElementById('filterInput');
+// Add event listener
 filterInput.addEventListener('keyup', filterNames);
 
 function filterNames(){
-    // Get value of input
+  // Get value of input
+  let filterValue = document.getElementById('filterInput').value.toUpperCase();
 
-    let filterValue = document.getElementById("filterInput").value.toUpperCase();
+  // Get names ul
+  let ul = document.getElementById('names');
+  // Get lis from ul
+  let li = ul.querySelectorAll('li.collection-item');
 
-    // Get names from ul
-    let ul = document.getElementById('names');
-    // Get lists from ul
-    let li = ul.querySelectorAll('li.collection-item');
-
-    //loop through collection-item lists
-    for(let i = 0;i<li.length;i++){
-        let a = li[i].getElementsByTagName('a')[0];
-        // If matched
-        if(a.innerHTML.toUpperCase().indexOf(filterValue)>-1){
-            li[i].style.display = '';
-        // If it doesnt match
-        }else{
-            li[i].style.display = 'none';
-        }
+  // Loop through collection-item lis
+  for(let i = 0;i < li.length;i++){
+    let a = li[i].getElementsByTagName('a')[0];
+    // If matched
+    if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+      li[i].style.display = '';
+    } else {
+      li[i].style.display = 'none';
     }
-   
+  }
+
 }
