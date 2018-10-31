3d47af255f984c343f462fc8be8e3fc0fb92202e
wahib <wahib.med.ouahib@gmail.com>
2018-10-31 17:20:18 +0000

    :construction: modifying JavaScript code

--- filterablelist/app.js
+++ filterablelist/app.js
@@ -13,8 +13,17 @@ function filterNames(){
     let ul = document.getElementById('names');
     // Get lists from ul
     let li = ul.querySelectorAll('li.collection-item');
-    console.log(li);
+
     //loop through collection-item lists
-    
+    for(let i = 0;i<li.length;i++){
+        let a = li[i].getElementsByTagName('a')[0];
+        // If matched
+        if(a.innerHTML.toUpperCase().indexOf(filterValue)>-1){
+            li[i].style.display = '';
+        // If it doesnt match
+        }else{
+            li[i].style.display = 'none';
+        }
+    }
    
 }
